8e0c465605f8b31cb6dd5ee50e0b7443060dfc2a
Andrii Chymyrys <andrii.chymyrys@gmail.com>
2022-07-10 15:18:51 +0300

    update DataSource

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     WideMorph\Morph\Bundle\MorphViewBundle\MorphViewBundle::class => ['all' => true],
     WideMorph\Morph\Bundle\MorphViewBlockBundle\MorphViewBlockBundle::class => ['all' => true],
     WideMorph\Morph\Bundle\MorphCoreBundle\MorphCoreBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
