51f4ac7de10b557775014ab073ce826e98292ad5
Andrii Chymyrys <andrii.chymyrys@gmail.com>
2022-11-18 18:18:22 +0200

    [App] add cms engine vue store

--- assets/js/App/Store.js
+++ assets/js/App/Store.js
@@ -2,10 +2,12 @@ import {createStore} from 'vuex'
 
 import CORE from '../../../vendor/widemorph/morph-view-bundle/Resources/assets/js/App/Store/Core/store';
 import DataImport from '../../../vendor/widemorph/morph-data-import-bundle/Resources/assets/js/App/Store/DataImport/store';
+import CmsEngine from '../../../vendor/widemorph/cms-engine-bundle/Resources/assets/js/App/Store/store';
 
 export default createStore({
     modules: {
         'core': CORE,
         'dataImport': DataImport,
+        'cmsEngine': CmsEngine,
     }
 })
